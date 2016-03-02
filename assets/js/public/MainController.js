624acfaf79bb67b325c926b0fc13dd6a005a18cc
Ed Davis <ecd5070@gmail.com>
2016-03-02 13:00:56 -0500

    add static recommended route

--- assets/js/public/MainController.js
+++ assets/js/public/MainController.js
@@ -1,6 +1,66 @@
-angular.module('HomeTown').controller('MainController', function($scope, $http, $filter) {
-
+angular.module('HomeTown').controller('MainController', function($scope, $http, $filter, uiGmapIsReady) {
 	//TODO dummy map object and marker (coordinates for 230 E Girard Ave Philadelphia, PA 19125)
+	
+	uiGmapIsReady.promise(1).then(function(instances) {
+		// note direction service cannot process TRANSIT requests with multiple waypoints, need to break up into multiple requests
+        instances.forEach(function(inst) {
+            var map = inst.map,
+				directionsService = new google.maps.DirectionsService(),
+				request1 = {
+					origin: '1421 N Howard St, Philadelphia, PA',
+					destination: '126 Elfreths Alley, Philadelphia, PA 19106',
+					travelMode: google.maps.TravelMode.TRANSIT
+				},
+				request2 = {
+					origin: '126 Elfreths Alley, Philadelphia, PA 19106',
+					destination: '520 Chestnut St, Philadelphia, PA 19106',
+					waypoints: [{
+						location: '6th St & Market St, Philadelphia, PA 19106', //liberty bell
+						stopover: true
+					}],
+					optimizeWaypoints: true,
+					travelMode: google.maps.TravelMode.WALKING
+				};
+			directionsDisplay1 = new google.maps.DirectionsRenderer({
+				map: map,
+				suppressMarkers: true
+			});
+			directionsDisplay2 = new google.maps.DirectionsRenderer({
+				map: map,
+				suppressMarkers: true
+			});
+
+			directionsService.route(request1, function(result, status) {
+				if (status == google.maps.DirectionsStatus.OK) {
+		      		directionsDisplay1.setDirections(result);
+		    	}
+			});
+			directionsService.route(request2, function(result, status) {
+				if (status == google.maps.DirectionsStatus.OK) {
+		      		directionsDisplay2.setDirections(result);
+		    	}
+
+		    	//add markers
+	      		var route = result.routes[0],
+	      		marker = new google.maps.Marker({
+      				position: route.legs[0].start_location,
+      				map: map,
+      				icon: icon
+      			}),
+      			icon = 'images/icons/icn-eye.svg';
+
+
+	      		for(var i=0; i<route.legs.length; i++) {
+	      			marker = new google.maps.Marker({
+	      				position: route.legs[i].end_location,
+	      				map: map,
+	      				icon: icon
+	      			});
+	      		}
+			});
+        });
+    });
+
 	$scope.map = { 
 		center: { 
 			latitude: 39.972530, 
@@ -91,6 +151,8 @@ angular.module('HomeTown').controller('MainController', function($scope, $http, 
         }
 	};
 
+
+/*
 	//TODO Dummy weather API call.. will move into service layer (using lat long for 230 E Girard Ave, Philadelphia, PA 19125 according to Google maps)
 	$http.get('/weather/current?lat=39.9691568&long=-75.1327133') //https://api.forecast.io/forecast/f0f643b626c0788853800bcc4570696a/39.9691568,-75.1327133
      .success(function(response){
@@ -104,10 +166,10 @@ angular.module('HomeTown').controller('MainController', function($scope, $http, 
      });
 
      //dummy Eventful API call
-     /*$http.get('/events/list?app_key=Lqg82zJw6bv2bDG7&location=Philadelphia')
+     $http.get('/events/list?app_key=Lqg82zJw6bv2bDG7&location=Philadelphia')
      	.success(function(response) {
      		debugger;
-     	});*/
+     });
 
 	//TODO temporary dummy data
 	$scope.showsList = [{
@@ -153,5 +215,5 @@ angular.module('HomeTown').controller('MainController', function($scope, $http, 
 		time: '10 PM',
 		ticketURL: 'http://ticketmaster.com'
 	}];
-
+*/
 });
